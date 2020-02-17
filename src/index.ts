692026ef9b1a887156700ba1335ddefbaf562412
Mikey Korst <mpk44@pitt.edu>
2020-02-17 20:53:33 +0000

    Finished 1st day. Added add functionality

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { add } from './math';
 
+const num1Elem = document.getElementById('num1') as HTMLInputElement;
+const num2Elem = document.getElementById('num2') as HTMLInputElement;
+const addButton = document.getElementById('add') as HTMLButtonElement;
+const answerElem = document.getElementById('answer') as HTMLElement;
+
+addButton.addEventListener('click', () => {
+    const a = num1Elem.valueAsNumber;
+    const b = num2Elem.valueAsNumber;
+
+    const sum = add(a, b);
+
+    answerElem.innerText = sum.toString();
+});
